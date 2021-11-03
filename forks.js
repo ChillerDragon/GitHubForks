152847a379c1afba308f67f34c997d5d861ad34c
ChillerDragon <ChillerDragon@gmail.com>
2021-11-03 09:42:38 +0100

    Refactor and fix errors on non github page

--- forks.js
+++ forks.js
@@ -16,6 +16,16 @@ const fetchApi = (url, repo) => {
 }
 */
 
+const isGithubNetworkPage = () => {
+  if (window.location.href.startsWith('https://github.com/')) {
+    return true
+  }
+  if (window.location.href.endsWith('network/members')) {
+    return true
+  }
+  return false
+}
+
 const getBranchHtml = (branch) => {
   const behind = branch.match(/^This branch is (\d+) commits? behind/)
   if (behind) {
@@ -33,8 +43,8 @@ const getBranchHtml = (branch) => {
 }
 
 const network = document.querySelector('.network')
-const repos = network.querySelectorAll('.repo')
-const rootRepo = network.querySelector('.repo')
+const repos = isGithubNetworkPage() ? network.querySelectorAll('.repo') : []
+const rootRepo = isGithubNetworkPage() ? network.querySelector('.repo') : []
 const sortedRepos = []
 let finishSort = false
 let numFetched = 0
@@ -50,6 +60,9 @@ const sortRepos = () => {
 }
 
 const checkSort = () => {
+  if (!isGithubNetworkPage()) {
+    return
+  }
   if (finishSort) {
     return
   }
@@ -67,52 +80,58 @@ const checkSort = () => {
 
 setInterval(checkSort, 500)
 
+const processRepo = (repo) => {
+  const aNode = repo.querySelectorAll('a')[2]
+  const url = aNode.href
+  fetch(url)
+    .then(data => data.text())
+    .then((html) => {
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(html, 'text/html');
+      const branchDom = doc.querySelector('.d-sm-flex.Box > .d-flex')
+      if (!branchDom) {
+        return // the root repo
+      }
+      const stars = doc.querySelectorAll('.social-count')[2].innerText
+      const branch = branchDom.innerText.trim()
+      const branchStr = getBranchHtml(branch)
+      // github style stars (looks ugly tho)
+      /*
+      const starsHtml = `
+      <span style="border: 1px solid rgb(136, 136, 136);border-radius: 3px">
+      <span style="background-color: rgb(247, 247, 247);border-right: 1px solid rgb(136, 136, 136);">
+      ${svgStar}
+      </span>
+      <span>
+      ${stars}
+      </span>
+      </span>
+      `
+      */
+      const svgStar = `
+      <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-star mr-1">
+        <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
+      </svg>
+      `
+      const starsHtml = `<span>(${stars} ${svgStar})</span>`
+      aNode.insertAdjacentHTML('afterend', ` <span>${branchStr} ${starsHtml}</span>`)
+      sortedRepos.push([parseInt(stars, 10), repo])
+      // console.log(`push node ${url} fteched=${numFetched}/${repos.length}`)
+      numFetched++
+    })
+}
+
 const renderForkInfo = () => {
-  if (!window.location.href.startsWith('https://github.com/')) {
+  if (!isGithubNetworkPage()) {
     return
   }
-  if (!window.location.href.endsWith('network/members')) {
-    return
-  }
-  const svgStar = `
-  <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-star mr-1">
-    <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
-  </svg>
-  `
+  let lastNestLevel = 1
   repos.forEach((repo) => {
-    const aNode = repo.querySelectorAll('a')[2]
-    const url = aNode.href
-    fetch(url)
-      .then(data => data.text())
-      .then((html) => {
-        const parser = new DOMParser()
-        const doc = parser.parseFromString(html, 'text/html');
-        const stars = doc.querySelectorAll('.social-count')[2].innerText
-        const branchDom = doc.querySelector('.d-sm-flex.Box > .d-flex')
-        if (!branchDom) {
-          return // the root repo
-        }
-        const branch = branchDom.innerText.trim()
-        const branchStr = getBranchHtml(branch)
-        // github style stars (looks ugly tho)
-        /*
-        const starsHtml = `
-          <span style="border: 1px solid rgb(136, 136, 136);border-radius: 3px">
-            <span style="background-color: rgb(247, 247, 247);border-right: 1px solid rgb(136, 136, 136);">
-              ${svgStar}
-            </span>
-            <span>
-              ${stars}
-            </span>
-          </span>
-        `
-        */
-        const starsHtml = `<span>(${stars} ${svgStar})</span>`
-        aNode.insertAdjacentHTML('afterend', ` <span>${branchStr} ${starsHtml}</span>`)
-        sortedRepos.push([parseInt(stars, 10), repo])
-        // console.log(`push node ${url} fteched=${numFetched}/${repos.length}`)
-        numFetched++
-      })
+    const nestLevel = repo.querySelectorAll('svg').length
+    if (nestLevel > lastNestLevel) {
+
+    }
+    processRepo(repo)
   })
 }
 
